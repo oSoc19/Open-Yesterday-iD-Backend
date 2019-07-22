e1e570ff428e60cbe7aa3c44aaccf03b4647103f
Richez Antoine <richez.antoine.richez@gmail.com>
2019-07-22 11:22:31 +0200

    Finalized the documentation'

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Start the server
+If it is your first use of this little 'backend', you first have to use the command `npm install` to install all the required packages to make this REST API work.
+
 You can start the server by typing `node server.js`
 
 # Use
@@ -9,3 +11,30 @@ Fetch a loginToken from wikimedia commons API, logs in with credentials (You can
 
 ## Upload
 Uploads the image, make the returned url pass through the bitly API to make it shorter and then returns it to your frontend
+
+## The bot.json credentials file
+In order to log in wikimedia and bitly you need credentials: for obvious security reasons, the original `bot.json` credentials file is not shared online. But you can create your own by registering an account at wikimedia and after create a bot password <a href="https://www.mediawiki.org/wiki/Special:BotPasswords">here</a>
+
+You also need an account for shortening links through bit.ly API. You can create an account <a href="https://bitly.com/a/sign_in">here</a>. Steps to get the API credentials below.
+
+Here is a template of the `bot.json` credentials file: 
+```js
+{
+    "loginInfo": {
+        "lgname": "<given wikimedia's bot login username>",
+        "lgpassword": "<given wikimedia's bot password>"
+    },
+    "wikimedia": {
+        "login": "<given bitly's login username>",
+        "api_key": "<given bitly's password>"
+    }
+}
+```
+
+## How to get the Bitly's API credentials
+Just follow the red arrows after creating your account and being on the bitly page.
+<img src="./readmePICS/bitlyUI.png">
+<img src="./readmePICS/bitlyAccountSettings.png">
+<img src="./readmePICS/bitlyACCOUNTDETAILS.png">
+<img src="./readmePICS/bitlyAdvancedSettings.png">
+<img src="./readmePICS/bitlyAPICredentials.png">
--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-const express = require('express');
-const request = require('request');
-var multer = require('multer');
-var port = process.env.PORT || 3000;
-const app = express();
-let apiURL = "https://commons.wikimedia.org/w/api.php"
-let shortenApiURL = "http://api.bitly.com/v3/shorten?callback=?"
-let CSRFToken;
-let jsonLogInfo = require('./bot.json');
+const express = require('express');                                 // Here express is used to make a REST API
+const request = require('request');                                 // Request is a simple alternative to https/http module, check the repo at: https://github.com/request/request 
+var multer = require('multer');                                     // Multer is used like `body-parser` to parse incoming formData request (a picture), check: https://github.com/expressjs/multer for more information
+var port = process.env.PORT || 3000;                                // The actual port used by the API.
+const app = express();                                              
+let apiURL = "https://commons.wikimedia.org/w/api.php"              // The HyperText link to the wikimedia API. You can check more about the API by clicking the link or by checking : https://www.mediawiki.org/wiki/API:Main_page
+let shortenApiURL = "http://api.bitly.com/v3/shorten?callback=?"    // The HyperText link to the bitly API. You can check more about the bit.ly API at: https://dev.bitly.com/
+let CSRFToken;                                                      // This is a token that permits us to upload a file/picture to the wikimedia commons wiki
+let jsonLogInfo = require('./bot.json');                            // This is the bot credentials json file. You can check the README.md file to see the template if you want to make your own
 let loginToken;
 let params;
 const upload = multer();
 
 app.get('/login', function(req,res){
-    // send a fetch request to get the login token
+    // send a request to get the login token, you can check more about the login process at: https://www.mediawiki.org/wiki/API:Login 
     params = "action=query&meta=tokens&format=json&type=login";
+    // for the request, we add some options like `credentials: 'include'` and `jar: 'true'` to keep the session (The session is maintained via the shared cookies)
     request.get({url: apiURL + "?" + params, credentials: 'include', jar: 'true'}, (err, result, body) => {
         body = JSON.parse(body);
         loginToken = body.query.tokens.logintoken;
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', '*');      // Here we set a header, meaning that the response can be shared with the requesting code from the given origin.
         login(res);
     });
 });
 
 app.post('/upload', upload.single('file'), function(req,res){
+    // This function is used when we want to upload a picture to wikimedia commons. You can check the documentation at: https://www.mediawiki.org/wiki/API:Upload 
     if(!CSRFToken){
         res.status(404).send('CSRFTokenNotFound');
     }
+    // This formData is required by the API, we create a custom file (the file field) because we have to stringify the incoming picture, we, then, can access its buffer and add options to the file
     let formData = {
         action: 'upload',
         format: 'json',
@@ -47,14 +50,16 @@ app.post('/upload', upload.single('file'), function(req,res){
 });
 
 app.use((err, req, res, next) => {
+    // This is our `error handler`, if an error code is thrown somewhere it will automatically come here and send the message to the host
     res.status(err.status || 500);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json(err.message);
 });
 app.listen(port);
-console.log("Server is listening to http://localhost:" + port);
+console.log("Server is listening to http://localhost:" + port);     // For the moment the API is used in localhost but if you plan to host it somewhere and want to keep a track of it just replace `localhost` by your IP/DN
 
 function login(res){
+    // This function is used like a callback after the fetch of the login token.
     if(loginToken) {
         let formData = {
             action: 'login',
@@ -72,16 +77,19 @@ function login(res){
     else res.status(404).send("No login token");
 }
 function getCSRFToken(res){
+    // This function is used as a callback to the login function to fetch a CSRF token used to upload an image to wikimedia
     params = "action=query&meta=tokens&format=json";
     request.get({url: apiURL + "?" + params, credentials: 'include', jar: 'true'}, (err,result,body) => {
         body = JSON.parse(body);
         CSRFToken = body.query.tokens.csrftoken;
         res.status(200).send('login successful');
-        console.log('login successful');
+        // You can add this log for testing purposes or if you want to keep track of login
+        //console.log('login successful');
     });
 }
 
 function shortenURL(longURL, res){
+    // This function is used as a callback of the upload to wikimedia to permits us to shorten them and then store more pictures. You can check the doc at: https://dev.bitly.com/links.html#v3_shorten 
     params = "format=json&apiKey=" + jsonLogInfo.wikimedia.api_key + "&login=" + jsonLogInfo.wikimedia.login + "&longUrl=" + longURL;
     request.get({url: shortenApiURL + params}, (err,result,body) => {
         body = JSON.parse(body);
